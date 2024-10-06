1d8ae98bb1e9f56f0d095b72aeeae9344865b613
jorge.donascimento <jorge.donascimento@apoio.agro.gov.br>
2024-10-06 04:14:03 -0300

    Refactor GeckoSimplePriceDto and update cron schedule.
    Add JsonAnySetter to GeckoSimplePriceDto for dynamic property setting. Adjust cron schedule in AssetJobService and fix formatting in StockQuotationHandler.

--- src/main/java/br/dev/jstec/tyginvestiment/clients/dto/GeckoSimplePriceDto.java
+++ src/main/java/br/dev/jstec/tyginvestiment/clients/dto/GeckoSimplePriceDto.java
@@ -1,14 +1,20 @@
 package br.dev.jstec.tyginvestiment.clients.dto;
 
+import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Data
 public class GeckoSimplePriceDto {
-    @JsonProperty
-    private Map<String, CurrencyData> currencies;
+    private Map<String, CurrencyData> currencies = new HashMap<>();
+
+    @JsonAnySetter
+    public void setCurrencyData(String name, CurrencyData currencyData) {
+        this.currencies.put(name, currencyData);
+    }
 
     @Data
     public static class CurrencyData {
