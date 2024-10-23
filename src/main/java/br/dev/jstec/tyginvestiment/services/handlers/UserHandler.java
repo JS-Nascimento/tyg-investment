4b29769734791fb6ef52841cff1e939b0d831729
Jorge Nascimento <jorge@jstec.dev.br>
2024-10-22 22:48:00 -0300

    feat(user): adicionar atributos de segurança na entidade User
    - Adicionar campos enabled, accountNonExpired, e isAccountNonLocked
    - Remover import desnecessário em UserMapper
    - Injetar PasswordEncoder em UserHandler
    - Codificar senha antes de salvar usuário

--- src/main/java/br/dev/jstec/tyginvestiment/services/handlers/UserHandler.java
+++ src/main/java/br/dev/jstec/tyginvestiment/services/handlers/UserHandler.java
@@ -5,6 +5,7 @@ import br.dev.jstec.tyginvestiment.models.User;
 import br.dev.jstec.tyginvestiment.repository.UserRepository;
 import br.dev.jstec.tyginvestiment.services.mappers.UserMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,9 +15,12 @@ public class UserHandler {
 
     private final UserRepository userRepository;
     private final UserMapper mapper;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public UserDto saveUser(UserDto dto) {
+
+        dto.setPassword(passwordEncoder.encode(dto.getPassword()));
         var entity = mapper.toEntity(dto);
 
         var user = userRepository.save(entity);
